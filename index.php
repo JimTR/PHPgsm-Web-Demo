7c71738ea9f3a8b24c055685ebee605d810533f6
Jim <jim@noideersoftware.co.uk>
2022-03-14 07:32:54 +0000

    Use in sub folder

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ require DOC_ROOT. '/inc/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define( 'SQ_ENGINE',      SourceQuery::SOURCE );
 	define( 'LOG',	'logs/ajax.log');
 $module = "Dashboard";	
-$build = "7953-1574912657";
+$build = "7128-391349453";
 $version = "1.010";
-$time = "1647242024";
+$time = "1643892940";
 
     $Auth = new Auth ();
     $user = $Auth->getAuth();
