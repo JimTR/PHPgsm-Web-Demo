d8c3ae8eee9c03556e2e13d7ec538a96b148594e
Jim <jim@noideersoftware.co.uk>
2022-05-29 07:20:07 +0100

    show load ave as %

--- js/baseserver.js
+++ js/baseserver.js
@@ -17,7 +17,7 @@
             $('#processors').text(data.processors);
             $('#cores').text(data.cpu_cores);
             $('#speed').text(data.cpu_MHz);
-            $('#load').text(data.load);
+            $('#load').text(data.load_pc);
             $('#cache').text(data.cache_size);
             $('#ip').text(data.ips);
             $('#reboot').text(data.reboot);
@@ -42,12 +42,12 @@
             $('#swapfree').text(data.SwapFree); 
             $('#u_mount').text(data.dir);
             $('#u_size').text(data.quota);
-            $('#load_1').text(data.load_1_min);
-            $('#load_10').text(data.load_10_min);
-            $('#load_15').text(data.load_15_min);
-            changeforeground('load_1',data.load_1_min_pc);
-			changeforeground('load_10',data.load_10_min_pc);
-			changeforeground('load_15',data.load_15_min_pc);
+            $('#load_1').text(data.load_1_min_pc);
+            $('#load_10').text(data.load_10_min_pc);
+            $('#load_15').text(data.load_15_min_pc);
+            changeforeground('load_1',parseInt(data.load_1_min_pc));
+			changeforeground('load_10',parseInt(data.load_10_min_pc));
+			changeforeground('load_15',parseInt(data.load_15_min_pc));
             if (data.hypervisor_vendor !==undefined){
 				//console.log(data.hypervisor_vendor);
 				$('#ctype').text(data.hypervisor_vendor);
