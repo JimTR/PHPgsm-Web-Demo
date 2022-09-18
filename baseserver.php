c4fa701ba4549d91f8150a99a069eb520ce8fb8c
Jim <jim@noideersoftware.co.uk>
2022-09-18 08:54:26 +0100

    test database disconnect

--- baseserver.php
+++ baseserver.php
@@ -112,5 +112,6 @@ $template->load('templates/baseserver.html');
 $template->replace_vars($page);
 $template->replace_vars($sdata);
 $template->publish();
+$database->disconnect();
 // background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23212529'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
 ?>
