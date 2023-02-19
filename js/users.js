d528fda139a9a79b2099a8cc16bf4cd47813714f
Jim <jim@noideersoftware.co.uk>
2023-02-19 10:16:02 +0000

    add link to top ten players

--- js/users.js
+++ js/users.js
@@ -153,8 +153,9 @@ $('#data_table').on('click','.tpButton', function(event) {
 					gendetail +='<tr><td>Steam Profile</td><td><a href=http://steamcommunity.com/profiles/'+gen_data.steam_id64+' target="_blank">'+gen_data.steam_id64+'</a></td></tr>';
 					console.log(gen_data);
 					gendetail +="<tr><td>Overall Time on Line</td><td>"+data.time_on_line+"</td></tr>";
-					if (gen_data.aka != null){
-						aka = gen_data.aka.replace(/,\s*$/, "");
+					aka = gen_data.aka;
+					if (aka != null){
+						aka = aka.replace(/,\s*$/, "");
 						gendetail +="<tr><td>Played as</td><td>"+aka+"</td></tr>";
 					}
 					}
