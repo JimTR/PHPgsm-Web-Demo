cd58b53d300093a0e198863b4cd7350822901f21
Jim <jim@noideersoftware.co.uk>
2023-03-24 08:39:14 +0000

    rework stats page

--- stats.php
+++ stats.php
@@ -27,11 +27,24 @@ $build = "1667-3072679804";
 $version = "1.001";
 $time = "1663578073";
 $module = "stats";
-//$user = $Auth->getAuth();
 $bserver = explode('=',$_SERVER['QUERY_STRING']);
 $we_are_here = $settings['url'];
 $url = $settings['url'];
-
+$sql = "select * from logins limit 1";
+$country = reset($database->get_results($sql));
+//reset($country);
+$page['country'] = $country['country'];
+$page['country_stat'] = "{$country['logins']}/{$country['players']}";
+$sql = "SELECT * FROM `user_stats`order by time_on_line desc, log_ons desc;";
+$stats = $database->get_results($sql);
+//die(print_r($stats,true));
+//reset($stats);
+$page['time_online'] = $stats[0]['name'];
+$page['time_online_count'] = convertSecToTime($stats[0]['time_on_line']);
+$columns = array_column($stats, 'total_log_ons');
+array_multisort($columns, SORT_DESC, $stats);
+$page['most_log_ons'] = $stats[0]['name'];
+$page['log_on_count'] = $stats[0]['total_log_ons'];
 $page['page-title'] = 'Statistics';
 $page['url'] = $url;
 $page['today'] = date("Y-m-d");
@@ -46,4 +59,27 @@ $page['sidebar'] = str_replace("class=\"nav-link collapsed\" id=\"$module\" ","c
 $template->load('templates/stats.html');
 $template->replace_vars($page);
 $template->publish();
+
+function convertSecToTime($sec){
+	$return='';
+	if (!is_numeric($sec)) {$sec=0;}
+	$date1 = new DateTime("@0"); //starting seconds
+	$date2 = new DateTime("@$sec"); // ending seconds
+	$interval =  date_diff($date1, $date2); //the time difference
+	$y =  $interval->format('%y');
+	$m = $interval->format('%m');
+	$d = $interval->format('%d');
+	$h = $interval->format('%H');
+	$mi = $interval->format('%I');
+	$s = $interval->format('%S');
+	if ($y >0) {$return.= "{$y}y, ";}
+	if ($m >0) {$return.= "{$m}m, ";}
+	//if ($m > 0 and $y == 0) {$return .= "$m mo ";}
+	if($d >0){$return .= "{$d}d, ";}
+	$return .= "$h:";
+	$return.= "$mi:";
+	$return .= "$s";	
+	//return $interval->format('%y y %m mo %d d, %h h %i m %s s'); // convert into Years, Months, Days, Hours, Minutes and Seconds
+	return $return;
+}
 ?>
