4c480104dd9e5ef722bd76ca08db3d1bda3e499c
Jim <jim@noideersoftware.co.uk>
2023-04-15 14:39:14 +0100

    messing with maps

--- maps.php
+++ maps.php
@@ -23,61 +23,67 @@
  */
  	include 'inc/master.inc.php';
 	$bserver = $_POST['server'];
-	$sql = "select * from server1 where host_name = '$bserver'";
-	$server =  $database->get_row($sql);
-	echo "{$server['location']}<br>";
-	$symlink_location = "{$server['location']}/{$server['game']}";
-	echo "this is symlink $symlink_location<br>";
-	$location = "{$server['install_dir']}/{$server['game']}";
-	if ($symlink_location === $location) {$symlink = false;} 	else {$symlink = true;}
-	if($symlink)  {echo "we should symlink<br>";}
-	echo $location;
-	//die();
-    $countfiles = count($_FILES['userfile']['name']);
-    echo "<br>file count $countfiles<br>";
 	
-    $totalFileUploaded = 0;
-    for($i=0;$i<$countfiles;$i++){
-         $filename = $_FILES['userfile']['name'][$i];
-
-         ## Location
-         //$location = "/usr/games/".$filename;
-         $extension = pathinfo($filename,PATHINFO_EXTENSION);
-         $extension = strtolower($extension);
-
-         ## File upload allowed extensions
-         $valid_extensions = array("bsp","nav","ain","bz2");
-		//echo "extension $extension<br>";
-         $response = 0;
-         ## Check file extension
-         if(in_array(strtolower($extension), $valid_extensions)) {
+	if ($_POST['action'] == 'upload') {
+		$sql = "select * from server1 where host_name = '$bserver'";
+		$server =  $database->get_row($sql);
+		$output[] =$server['location'];
+		$symlink_location = "{$server['location']}/{$server['game']}";
+		$output[] = "this is symlink $symlink_location";
+		$location = "{$server['install_dir']}/{$server['game']}";
+		if ($symlink_location === $location) {$symlink = false;} 	else {$symlink = true;}
+		if($symlink)  {$output[] = "we should symlink";}
+		$output[] = $location;
+		$countfiles = count($_FILES['userfile']['name']);
+		$output[] = "file count $countfiles";
+		$totalFileUploaded = 0;
+		for($i=0;$i<$countfiles;$i++){
+			$filename = $_FILES['userfile']['name'][$i];
+			$extension = pathinfo($filename,PATHINFO_EXTENSION);
+			$extension = strtolower($extension);
+			## File upload allowed extensions
+			$valid_extensions = array("bsp","nav","ain","bz2");
+			$response = 0;
+			## Check file extension
+			if(in_array(strtolower($extension), $valid_extensions)) {
               ## Upload file
-              echo "extension $extension<br>";
-              switch ($extension) {
-				  // work out where to put them
-				  case "bsp":
-				  case "nav":
-				  	$upload_location = "$location/maps/$filename";
-					$symlink_upload = "$symlink_location/maps/$filename";
-					echo "$upload_location<br>";
-				case "ain":	
-					//$location  ="";
-			  }
-              if(move_uploaded_file($_FILES['userfile']['tmp_name'][$i],$upload_location)){
-				  if($symlink) {
-					  echo "linking $upload_location to $symlink_upload<br>";
-					symlink($upload_location,$symlink_upload); // doesn't work 100%
-					//shell_exec("ln -s $upload_location $symlink_upload"); // this does
-					 
+				$output[] = "extension $extension";
+				switch ($extension) {
+					// work out where to put them
+					case "bsp":
+					case "nav":
+						$upload_location = "$location/maps/$filename";
+						$symlink_upload = "$symlink_location/maps/$filename";
+						$output[] = "$upload_location";
+					case "ain":	
+						$upload_location = "$location/maps/graphs/$filename";
+						$symlink_upload = "$symlink_location/maps/graphs/$filename";
+					case "bz2":
 				}
-
-                   echo "file name : ".$filename."<br/>";
-
+				if(move_uploaded_file($_FILES['userfile']['tmp_name'][$i],$upload_location)){
+					if($symlink) {
+						$output[] = "linking $upload_location to $symlink_upload";
+						symlink($upload_location,$symlink_upload); 
+					}
+                   $output[] = "file name : $filename";
                    $totalFileUploaded++;
-              }
-         }
-
-    }
-    echo "Total File uploaded : ".$totalFileUploaded;
-
-
+				}
+			}
+		}
+		$output[] = "Total File uploaded : $totalFileUploaded";
+		$view = json_encode($output);
+		echo $view;
+	}
+	if ($_POST['action'] == 'mapcycle') {
+		$maps = $_POST;
+		unset($maps['action']);
+		unset($maps['server']);
+		$sql = "select * from server1 where host_name = '$bserver'";
+		$server =  $database->get_row($sql);
+		$file_to_write = "{$server['location']}/{$server['game']}/cfg/mapcycle.txt";
+		unlink($file_to_write);
+		foreach ($maps as $k => $v) {
+			log_to($file_to_write,trim($k));
+		}
+		echo json_encode("map cycle updated");
+	}
