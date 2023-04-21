c0010a36adfc34e62406736568cba5d4f91e3932
Jim <jim@noideersoftware.co.uk>
2023-04-21 08:39:20 +0100

    check out ptot

--- js/index.js
+++ js/index.js
@@ -12,15 +12,8 @@ function index() {
 			$('#logins_tot').text(data1.logins_tot);
 			$('#player').text(data1.players);
 			$('#run_tot').text(data1.run_tot);
-			//$('#player_tot').text(data1.player_tot);
-			//$('#logins_tot').text(data1.logins_tot);
-			//$('#players').text(data1.players);
-			//$('#run_tot').text(data1.run_tot);
-			//alert (data1.players);
 		},
         complete:function(data1){
-			    //setTimeout(index(url),7000);
-				
 		}
     });
 }
@@ -30,8 +23,7 @@ function game_server(url,server) {
 	$.ajax({ 
         type: 'GET', 
         url: url, 
-        data    : { module: 'gameserver',
-						 server: server},
+        data    : { module: 'gameserver', server: server},
         dataType: "json", 
         success: function (data1) {
 			// got data
@@ -43,22 +35,22 @@ function game_server(url,server) {
 			//alert (data1.players);
 		},
         complete:function(data1){
-			    //setTimeout(index(url),3000);
-				
+			//setTimeout(index(url),3000);
 		}
-});
+	});
 }
 function online(url){
-        var timer =sessionStorage.getItem(url);
-        //console.log("enter online with "+url);
+     var timer =sessionStorage.getItem(url);
+     //console.log("enter online with "+url);
 	 $.ajax({
 		url: url,
 		type: 'post',
 		dataType: "json" ,
 		success: function(xml,status){
+			var ptot= 0;
+			var ps = '';
 			var data1 = xml;
 			for (var i in data1) {
-				//console.log(data1);
 				var fname = i; // got the base server
 				if( i =='general' ) {
 					var general = data1[i];
@@ -67,14 +59,9 @@ function online(url){
 						if( g =='countries') {
 							var country_data = general['countries'];  
 						}
-						//if(g == 'top_players') {
-						//	var top_players = general['top_players'];
-						//}
 					} 
 				}
-				if (i =='top_players') {
-					var top_players=data1[i];
-				}
+				if (i =='top_players') {var top_players=data1[i];}
 				//console.log(country_data);
 				for (var j in country_data) {
 					var td_title = '<tr id="country_'+j+'"><td id="country_row_'+j+'" title = "'+country_data[j]['country']+'"><img src="'+country_data[j]['flag']+'" style="height:18px;margin-top:-2px;"></td>';
@@ -98,9 +85,8 @@ function online(url){
 					var tr = td_title+td_first_logon+td_logons+td_last_logon;
 					$("td#player_row_"+j).parent().replaceWith(tr);   
 					//console.log(tr);
-					
 				}
-				for (var j in data1[i]) {
+			for (var j in data1[i]) {
 				// we have the individal server
 				if (typeof serverlength === 0) {
 					//console.log('server not set');
@@ -109,13 +95,7 @@ function online(url){
 				var server = data1[i][j]; // got server id
 				var server_id = j;	
 				if (server.running == 1 ) {
-					//console.log (general[g]+' '+g );
-					//$('#demo').append('<p>'+g+' '+general[g]+'</p>');
-					//if( g =='players') {
-					//		var player_tots = general['players'];  
-					//	}
 				} 
-				//return;
 			}
 			//console.log(player_tots);
 			if(typeof player_tots != 'undefined'){
@@ -126,38 +106,27 @@ function online(url){
 				$('#tinstalled').text(player_tots.game_tot);
 				$('#run_tot').text(player_tots.run_tot);
 			}
-			//$('#logins_tot').text(player_tots.tot_logins);
 			
-			//$('#run_tot').text(data1.general.players.run_tot);	
 			for (var j in data1[i]) {
-			// we have the individal server
-			if (typeof serverlength === 0) {
-				//console.log('server not set');
-				return;
-			}   
-			var server = data1[i][j]; // got server id
-			var server_id = j;	
-			if (server.running == 1 ) {
+				if (typeof serverlength === 0) {return;}   
+				var server = data1[i][j]; // got server id
+				//console.log(server);
+				var server_id = j;	
+				if (server.running == 1 ) {
 					var playern = server.Players;
+					
 					$('#pl'+server_id).html(playern); 
 					$("#"+server_id).show();
-					//var start_date = timeConverter(parseFloat(server.starttime));
-					//var rel_date = timeDifference(Math.floor(Date.now() / 1000), parseFloat(server.starttime));
-					//console.log("Current Time stamp "+Math.floor(Date.now() / 1000));
-					//console.log("Start Time "+server.r_time);
-					//console.log(rel_date);
 					var logo  =server.url+':'+server.bport+'/'+server.logo;
-					if (typeof server.Players === "undefined") {
-						server.Players = 0;
-					}   
+					if (typeof server.Players === "undefined") {server.Players = 0;}   
 					$("#img"+server_id).attr("src",logo);
 					$('#cmap'+server_id).html(server.Map);
 					$('#host'+server_id).html(server.server_name);
 					$('#gdate'+server_id).html(server.r_time);
 					real_players = server.Players - server.Bots;
+					ptot +=real_players;
 					$('#gol'+server_id).html(real_players+'/'+server.max_players);
 					$('#pt'+server_id).html(server.player_tot);
-						 
 					if (server.Players ==0 ) {
 						//console.log ('should be nowt '+server.Players);
 						$("#ops"+server_id).slideUp();
@@ -168,101 +137,84 @@ function online(url){
 						if (typeof server.players === "undefined") {
 							console.log('no need players array not set '+server.server_name);
 							return;
-					}   
-					$('#op1'+server_id).css('cursor','pointer');
-					$('#gol'+server_id).addClass('p_count');	
-					$("#pbody"+server_id).empty();
-					//var players = server.players;
-					var players = $.map(server.players, function(value, index) { return [value]; });
-					var players = players.sort((b, a) => (a.Frags > b.Frags) ? 1 : -1)
-					//console.log(players);
-					for (p in players) {
+						}   
+						$('#op1'+server_id).css('cursor','pointer');
+						$('#gol'+server_id).addClass('p_count');	
+						$("#pbody"+server_id).empty();
+						//var players = server.players;
+						var players = $.map(server.players, function(value, index) { return [value]; });
+						var players = players.sort((b, a) => (a.Frags > b.Frags) ? 1 : -1)
+						//console.log(players);
+						console.log("ptot moved = "+ptot);	   
+						for (p in players) {
 							newRowContent='<tr style="font-size:14px;"><td style="width:60% !important;"><i class="p_name">'+players[p].Name+'</i></td><td style="text-align:right;width:15%; !important;padding-right:14%" class="p_score">'+players[p].Frags+'</td><td style=text-align:right;padding-right:3%;width:20%" class="p_time">'+players[p].TimeF+'</td></tr>'; 
 							$("#pbody"+server_id).append(newRowContent);
 						}
 					}
+					
 				}
 				else {
 					$('#'+server_id).hide(); // hide the server
 				}					
-			}	
+			}
+			
 	}
-	    
+	 
   },
     fail: function() {
-		//Something went wrong. Inform user/try again as appropriate
-        alert('Failed');
-        //setTimeout('Update()', 2000);
+		 alert('Failed');
     },
 	complete:function(data,data1){
-		//if(timer === null){ console.log('timer not set')};
 		sessionStorage.setItem(url, "value");
-  }
+	}
   });
 }
 function timeConverter(UNIX_timestamp){
-  var a = new Date(UNIX_timestamp * 1000);
-  var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
-  var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var day = weekday[a.getDay()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var timeOfDay = ( hour < 12 ) ? "am" : "pm"; 
-  currentHours = ( hour > 12 ) ? hour - 12 : hour;
-     // Convert an hours component of "0" to "12"
-    
-  //hour = ( currentHours == 0 ) ? 12 : currentHours;
- 
-  //var date = dateOrdinal(date);
-  var d = a.getDate();
+	var a = new Date(UNIX_timestamp * 1000);
+	var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+	var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+	var year = a.getFullYear();
+	var month = months[a.getMonth()];
+	var day = weekday[a.getDay()];
+	var date = a.getDate();
+	var hour = a.getHours();
+	var timeOfDay = ( hour < 12 ) ? "am" : "pm"; 
+	currentHours = ( hour > 12 ) ? hour - 12 : hour;
+	var d = a.getDate();
 	var d1 =a.getHours();
-//console.log(currentHours);
-var d = ('0'+d).slice(-2);
+	var d = ('0'+d).slice(-2);
 	var m = a.getMonth()+1;
-var m = ('0'+m).slice(-2);
-
-var hour =('0'+hour).slice(-2);
-        //m += 1;  // JavaScript months are 0-11
-	var y = a.getFullYear();
-  var min = ('0'+a.getMinutes()).slice(-2);
-  var sec = a.getSeconds();
-
-  var time = d+ '-' + m + '-' + y + ' ' + hour + ':' + min ;
-  return time;
+	var m = ('0'+m).slice(-2);
+	var hour =('0'+hour).slice(-2);
+    var y = a.getFullYear();
+	var min = ('0'+a.getMinutes()).slice(-2);
+	var sec = a.getSeconds();
+	var time = d+ '-' + m + '-' + y + ' ' + hour + ':' + min ;
+	return time;
 }
 
 function timeDifference(current, previous) {
-    
-    var msPerMinute = 60 * 1000;
+	var msPerMinute = 60 * 1000;
     var msPerHour = msPerMinute * 60;
     var msPerDay = msPerHour * 24;
     var msPerMonth = msPerDay * 30;
     var msPerYear = msPerDay * 365;
-    
     var elapsed = current - previous;
-    
     if (elapsed < msPerMinute) {
-         return Math.round(elapsed/1000) + ' seconds ago';   
+		return Math.round(elapsed/1000) + ' seconds ago';   
     }
-    
     else if (elapsed < msPerHour) {
          return Math.round(elapsed/msPerMinute) + ' minutes ago';   
     }
-    
     else if (elapsed < msPerDay ) {
          return Math.round(elapsed/msPerHour ) + ' hours ago';   
     }
-
     else if (elapsed < msPerMonth) {
          return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';   
     }
-    
     else if (elapsed < msPerYear) {
          return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';   
     }
-    
     else {
          return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';   
     }
