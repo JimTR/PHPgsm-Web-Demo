a122ce2f952d3af7926a2499d0510331710dcc50
Jim <jim@noideersoftware.co.uk>
2023-05-25 06:49:01 +0100

    try this

--- helpers/country.php
+++ helpers/country.php
@@ -1,8 +1,10 @@
 <?php
 include "../inc/master.inc.php";
 $id= $_GET['id'];
-$sql = "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id' limit 1000";
-$rows = $database->num_rows( "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id'");
+if (!isset($_GET['page'])) {$page = 1;}
+else {$page = $_GET['page'];}
+$sql = "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id'";
+$rows = $database->num_rows( $sql);
 if ($rows > 999) {
 	$pages= ceil($rows/1000);
 }
@@ -10,8 +12,12 @@ else {
 	$pages = 1;
 }
 $r = $database->get_results($sql);
+switch ($page) {
+	case 1:
+		$data['country'] = array_slice($r,0,1000);
+	}
 $data['rows'] =  $rows;
 $data['pages'] = $pages;
-$data['county'] = $r;
+//$data['county'] = $r;
 echo json_encode($data);
 //echo json_encode($id);
