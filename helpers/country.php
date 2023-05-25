e34ad9387a29c65df4e2e22519d93e170cc6c853
Jim <jim@noideersoftware.co.uk>
2023-05-25 06:32:30 +0100

    try this

--- helpers/country.php
+++ helpers/country.php
@@ -3,7 +3,7 @@ include "../inc/master.inc.php";
 $id= $_GET['id'];
 $sql = "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id' limit 1000";
 $rows = $database->num_rows($sql);
-if ($rows > 1000) {
+if ($rows > 999) {
 	$pages= $rows/1000;
 }
 else {
