53b4888664502b3714543ccdf544889fad1912cf
JimTR <jim@noideersoftware.co.uk>
2023-05-25 06:16:57 +0100

    after tests expand this

--- helpers/country.php
+++ helpers/country.php
@@ -1,8 +1,9 @@
 <?php
 include "../inc/master.inc.php";
 $id= $_GET['id'];
-$sql = "select * from players where country_code like '$id'";
+$sql = "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id' limit 1000";
 //echo "$sql<br>";
 $r = $database->get_results($sql);
-echo json_encode($r);
+//printr($r);
+echo json_encode($database->escape($r));
 //echo json_encode($id);
