f62f5b2e8128b75f107a8c763a4875df71f2e7a6
Jim <jim@noideersoftware.co.uk>
2023-05-25 06:36:51 +0100

    try this

--- helpers/country.php
+++ helpers/country.php
@@ -4,7 +4,7 @@ $id= $_GET['id'];
 $sql = "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id' limit 1000";
 $rows = $database->num_rows( "select steam_id64,name_c,aka,server,first_log_on,last_log_on,log_ons,city from players where country_code like '$id'");
 if ($rows > 999) {
-	$pages= $rows/1000;
+	$pages= ceil($rows/1000);
 }
 else {
 	$pages = 1;
