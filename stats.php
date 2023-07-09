277c8120af91a2240921c4a804413dc71c48b890
Jim <jim@noideersoftware.co.uk>
2023-07-09 07:49:11 +0100

    add source ban data

--- stats.php
+++ stats.php
@@ -194,7 +194,7 @@ foreach ($servers as $server) {
 		$tmp = explode(" ",$system_ban);
 		printr($tmp);
 		//sleep(1);
-		//$steam_id = new SteamID(trim($tmp[2]));
+		$steam_id = new SteamID(trim($tmp[2]));
 		//$id64 = $steam_id->ConvertToUInt64();
 		//echo $tmp[2].'<br>';
 		$unit1['id'] = $id64;
