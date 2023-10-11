a75dcb489658660c222fcf27b4670fa18dddcf9c
Jim <jim@noideersoftware.co.uk>
2023-10-11 05:24:55 +0100

    fix broken logout !

--- logout.php
+++ logout.php
@@ -2,30 +2,13 @@
 $build = "672-2441115016";
 $version = "1.000";
 $time = "1643265477";
-    //require 'inc/master.inc.php';
-define('DOC_ROOT', realpath(dirname(__FILE__)));
-require DOC_ROOT . '/inc/functions.inc.php';  // spl_autoload_register() is contained in this file
-    require DOC_ROOT. '/inc/config.php'; // get config
-    include DOC_ROOT. '/inc/settings.php'; // get settings 
-    include DOC_ROOT.'/inc/functions.lin.php'; // linux os functions there will be a windoze version functions.win.php
-         const SALT = 'insert some random text here';
-          $db_settings = $config['database']; // load default db connection settings
-    $database = new db($db_settings); // start up a db class just in case we need it
-
-    $template = new Template;
-$Auth = new Auth ();
-        $user = $Auth->getAuth();
-print_r($user);
-//die();
-    //$filename = "user".$Auth->id.".txt";
-    //$old = getcwd(); // Save the current directory
-    //chdir(DOC_ROOT."/forum");
-    //unlink($filename);
-    //chdir($old); // Restore the old working directory    
-    //$Auth->loginUrl = "";
-    //$kill = $Auth->nid;
-    //distroy_session($kill,$database);
-    $Auth->logout();
+ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+    require 'inc/master.inc.php';
+	$Auth = new Auth ();
+	$user = $Auth->getAuth();
+	$Auth->logout();
 //die();
     $pms="0";	
     //die ("got here ".$site->settings['url'].'/index.php');
