5c71ab62a08a644d04121e28aca3d74506c86629
Jim <jim@noideersoftware.co.uk>
2024-03-13 12:55:32 +0000

    reduce sql usage

--- inc/master.inc.php
+++ inc/master.inc.php
@@ -173,8 +173,10 @@ foreach ($base_servers as $server) {
 	$uri = parse_url($server['url']);
 	$url = $uri['scheme']."://".$uri['host'].':'.$server['port'];
 	if(isset($uri['path'])) {$url .= "/".$uri['path'];}
-	if($server['master']){$page['jsa'] .= '"'.$url.'/api.php?action=game_detail&server='.$server['fname'].'",';}
-	else {$page['jsa'] .= '"'.$url.'/api.php?action=game_detail&filter=nogeneral&server='.$server['fname'].'",';}
+	//if($server['master']){
+		$page['jsa'] .= '"'.$url.'/api.php?action=game_detail&server='.$server['fname'].'",';
+	//}
+	//else {$page['jsa'] .= '"'.$url.'/api.php?action=game_detail&filter=nogeneral&server='.$server['fname'].'",';}
 }
 //if (str_ends_with(rtrim($jsa), ',') {$jsa = rtrim($jsa,",");} 
 $sidebar_data['servers'] = 'Game Servers';
